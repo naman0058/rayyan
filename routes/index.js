ee360100fd743dda1b4e9f3aa758e4870a55b99b
naman0058 <jnaman345@gmail.com>
2022-08-12 03:37:47 +0530

    a

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/contact-us', function(req, res, next) {
+  res.render('contactus', { title: 'Express' });
+});
+
+router.get('/jobs', function(req, res, next) {
+  res.render('jobs', { title: 'Express' });
+});
+
+router.get('/countries', function(req, res, next) {
+  res.render('countries', { title: 'Express' });
+});
+
+
+router.get('/visa', function(req, res, next) {
+  res.render('visa', { title: 'Express' });
+});
+
+
+router.get('/courses', function(req, res, next) {
+  res.render('courses', { title: 'Express' });
+});
+
 module.exports = router;
